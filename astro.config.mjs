e4dd94375609c02bc2678bfb1bc6387c0260f995
nao <>
2025-01-31 18:28:58 +0900

    mod: config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://nao-amj.github.io//',
+    outDir: "./dist",
+    base: 'for_x_mixi2/',
 	integrations: [mdx(), sitemap()],
 });
