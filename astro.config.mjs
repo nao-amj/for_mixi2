5557816f188f007e8abed59e370ba894a26d58df
nao <153602757+nao-amj@users.noreply.github.com>
2025-02-14 18:08:08 +0900

    Update astro.config.mjs
    base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://nao-amj.github.io//',
     outDir: "./dist",
-    base: 'for_x_mixi2/',
+    base: '/for_x_mixi2',
 	integrations: [mdx(), sitemap()],
 });
